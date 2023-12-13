30899fa1a11134d2020236c55793e30dfe806cbe
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-12-12 19:07:41 -0500

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -37,6 +37,14 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.add('bob', [24, 25], 0, false, isPlayer);
+		animation.add('angrybob', [26, 27], 0, false, isPlayer);
+		animation.add('hellbob', [28, 29], 0, false, isPlayer);
+		animation.add('ron', [30, 31], 0, false, isPlayer);
+		animation.add('glitched-bob', [34, 35], 0, false, isPlayer);
+		animation.add('gloop-bob', [32, 33], 0, false, isPlayer);
+		animation.add('little-man', [36, 37], 0, false, isPlayer);
+		animation.add('pizza', [38, 39], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
