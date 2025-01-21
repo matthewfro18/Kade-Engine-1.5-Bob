2226ab3da0b23e8ed12a7928b2800bd2e4688c3e
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-01-21 05:07:00 -0500

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -16,7 +16,7 @@ haxelib install hscript
 haxelib install hxdiscord_rpc 2.5.1
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
-haxelib git linc-luajit https://github.com/nebulazorua/linc_luajit
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/ashea-code/faxe
 haxelib install actuate
